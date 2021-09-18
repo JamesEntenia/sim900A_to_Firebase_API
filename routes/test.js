00f4eb64b9543b37f650711725786acfc62ecf61
James Entenia <jamesentenia23@gmail.com>
2021-09-18 13:00:07 +0800

    test

--- routes/test.js
+++ routes/test.js
@@ -26,7 +26,7 @@ var firebase_ref = firebaseDB.ref('Barangay');
 
 //----------------------------------HTTP GET------------------------------------
 router.get('/http_get',(req,res)=>{
-    res.send(typeof req.query)
+    res.send(req.query)
     firebase_ref.child('test').set(req.query);
 })
 //------------------------------------------------------------------------------
